d8591ff3c8a929dbae71313fa7ae4e538e2d5fbb
aniketchougule22 <chouguleaniket03@gmail.com>
2023-03-23 13:17:41 +0530

    adding infinite scroll & top loading bar

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,14 @@ export class NewsItem extends Component {
     return (
       <div className='my-2'>
         <div className="card">
-          <span className='position-absolute top-0 translate-middle badge rounded-pill bg-danger' style={{left: '85%', zIndex: '1'}}>{source}</span>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            right: '0'
+          }}>
+            <span className='rounded-pill bg-danger' style={{color: 'white'}}>{source}</span>
+          </div>
           <img src={!imageUrl ? "https://stimg2.cardekho.com/images/carNewsimages/userimages/650X420/30565/1679295548921/ElectricCar.jpg" : imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
